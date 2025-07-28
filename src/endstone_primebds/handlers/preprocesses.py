c54361bdb98998851db9d2941dd2ec4cba7e220a
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-07-28 12:09:53 -0400

    feat: mutes stop /msg cmds

--- src/endstone_primebds/handlers/preprocesses.py
+++ src/endstone_primebds/handlers/preprocesses.py
@@ -1,7 +1,7 @@
-from endstone import ColorFormat
 from endstone.event import PlayerCommandEvent, ServerCommandEvent
 from typing import TYPE_CHECKING
 
+from endstone_primebds.handlers.chat import handle_mute_status
 from endstone_primebds.utils.configUtil import load_config
 from endstone_primebds.utils.dbUtil import UserDB
 from endstone_primebds.utils.internalPermissionsUtil import check_internal_rank
@@ -21,21 +21,6 @@ def handle_command_preprocess(self: "PrimeBDS", event: PlayerCommandEvent):
 
     if config["modules"]["discord_logging"]["commands"]["enabled"]:
         discordRelay(f"**{player.name}** ran: {command}", "cmd")
-
-    # /me Crasher Fix
-    blocked_cmds = {"me", "tell", "w", "msg"}
-    if cmd in blocked_cmds and command.count("@e") >= 5:
-        for perm in ["minecraft.command.me", "minecraft.command.tell", "minecraft.command.w", "minecraft.command.msg"]:
-            event.player.add_attachment(self, perm, False)
-        event.is_cancelled = True
-
-        # Log the staff message
-        if config["modules"]["me_crasher_patch"]["enabled"]:
-            if config["modules"]["me_crasher_patch"]["ban"]:
-                self.server.dispatch_command(self.server.command_sender, f"tempban {player.name} 7 day Crasher Exploit")
-            else:
-                log(self, f"Player §e{player.name} §6was kicked due to §eCrasher Exploit", "mod")
-                player.kick("Crasher Detected")
 
     # Internal Permissions Handler
     if ((db.get_online_user(player.xuid).internal_rank == "Operator" and not player.has_permission("minecraft.kick")) or
@@ -89,6 +74,35 @@ def handle_command_preprocess(self: "PrimeBDS", event: PlayerCommandEvent):
         self.server.dispatch_command(self.server.command_sender, f"setrank \"{args[1]}\" default")
         event.is_cancelled = True
         return False
+
+    # /me Crasher Fix
+    abused_cmds = {"me", "tellraw", "tell", "w", "msg"}
+    msg_cmds = {"tellraw", "tell", "w", "msg"}
+    if cmd in abused_cmds and command.count("@e") >= 5:
+        for perm in ["minecraft.command.me", "minecraft.command.tellraw", "minecraft.command.tell", "minecraft.command.w", "minecraft.command.msg"]:
+            event.player.add_attachment(self, perm, False)
+        event.is_cancelled = True
+
+        # Log the staff message
+        if config["modules"]["me_crasher_patch"]["enabled"]:
+            if config["modules"]["me_crasher_patch"]["ban"]:
+                self.server.dispatch_command(self.server.command_sender, f"tempban {player.name} 7 day Crasher Exploit")
+            else:
+                log(self, f"Player §e{player.name} §6was kicked due to §eCrasher Exploit", "mod")
+                player.kick("Disconnected")
+
+    # Social Spy
+    if cmd in msg_cmds and args:
+
+        if db.get_mod_log(player.xuid).is_muted:
+            handle_mute_status(player)
+            event.is_cancelled = True
+            return True
+
+        message = " ".join(args[2:])
+        for pl in self.server.online_players:
+            if int(db.get_online_user(pl.xuid).enabled_ss) == 1:
+                pl.send_message(f"§8[§r{player.name} §7-> §r{args[1]}§8] §7{message}")
 
     db.close_connection()
 
