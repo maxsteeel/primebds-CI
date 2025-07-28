165576bbcdd70e74f382c96b60b8ebc62f54a34c
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-07-28 13:19:51 -0400

    fix: support hostnames

--- src/endstone_primebds/utils/addressUtil.py
+++ src/endstone_primebds/utils/addressUtil.py
@@ -1,11 +1,21 @@
+import re
 import socket
 
 def is_valid_ip(ip: str) -> bool:
+
+    # Validate IPv4
     try:
         socket.inet_aton(ip)
         return True
     except socket.error:
-        return False
+        pass
+
+    # Validate hostname (RFC 1123)
+    hostname_regex = re.compile(
+        r"^(?=.{1,253}$)(?!-)[A-Za-z0-9-]{1,63}(?<!-)"
+        r"(?:\.(?!-)[A-Za-z0-9-]{1,63}(?<!-))*\.?$"
+    )
+    return bool(hostname_regex.match(ip))
 
 def is_valid_port(port_str: str) -> bool:
     if not port_str.isdigit():
