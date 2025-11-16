0cf5203f11c8768f630b8a26af90568f9089cc94
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-11-16 13:28:48 -0500

    fix: discord command reference

--- src/endstone_primebds/commands/Message/discord.py
+++ src/endstone_primebds/commands/Message/discord.py
@@ -17,5 +17,5 @@ command, permission = create_command(
 
 config = load_config()
 def handler(self: "PrimeBDS", sender: CommandSender, args: list[str]) -> bool:
-    sender.send_message(f"{config['modules']['discord']['discord_command']}")
+    sender.send_message(f"{config['modules']['discord']['command']}")
     return True
